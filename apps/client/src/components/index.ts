99e8c4d2dbc4735232cccfcb667c5590d14c90ec
mohammad <mysideactivities@gmail.com>
2022-04-24 14:51:18 +0430

    merge conflicts

--- apps/client/src/components/index.ts
+++ apps/client/src/components/index.ts
@@ -1,14 +1,9 @@
 export * from './HomePage';
 export * from './Layout';
 export * from './organisms';
-<<<<<<< HEAD
-export * from './AdminPanelLayout';
-export * from './AdminPanelDashboardPage';
 export * from './LoginPage';
 export * from './PlanShowPage';
-=======
 export * from './AdminPanelLayout'
 export * from './AdminPanelDashboardPage'
 export * from './AdminPanelPlansPage'
 export * from './AdminPanelCreatePlanPage'
->>>>>>> 48ce76cb376fd1d1181c734f7f8b3c0dd94601f3
--- apps/client/src/elements/Div/index.ts
+++ apps/client/src/elements/Div/index.ts
@@ -2,11 +2,7 @@ export * from './Div';
 export * from './Layout';
 export * from './HomePage';
 export * from './PlanCard';
-<<<<<<< HEAD
-export * from './AdminPanelLayout';
 export * from './LoginCard';
 export * from './PlanShowPage';
-=======
 export * from './AdminPanelLayout'
 export * from './InputGroup'
->>>>>>> 48ce76cb376fd1d1181c734f7f8b3c0dd94601f3
