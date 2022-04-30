f7238f5dbc0699c107ca11795709616fb57273e5
Syd Amir <hemedani@gmail.com>
2022-04-30 16:43:02 +0430

    add temporary filter for getPlans

--- apps/server/src/dynamics/plan/getPlans.type.ts
+++ apps/server/src/dynamics/plan/getPlans.type.ts
@@ -1,5 +1,13 @@
 import FastestValidator from "https://esm.sh/fastest-validator@1";
-import { planSelectable, PuPlan, RPlan } from "../../schemas/mode.ts";
+import {
+  planSelectable,
+  PLANTYPE,
+  PLATETYPE,
+  POSITION,
+  PuPlan,
+  RPlan,
+  UNITTYPE,
+} from "../../schemas/mode.ts";
 
 import {
   basePaginationValidationObject,
@@ -19,6 +27,25 @@ export const schema = {
             "updateAt",
           ),
         },
+        planType: { type: "enum", values: ["Resindental", "Villa"] },
+        units: { type: "number", min: 1, max: 1000 },
+        floors: { type: "number", min: 1, max: 1000 },
+        sleeps: { type: "number", min: 1, max: 1000 },
+        bathroom: { type: "number", min: 1, max: 1000 },
+        planCode: { type: "string", min: 6, max: 10 },
+        unitType: {
+          type: "enum",
+          values: ["Solo", "Duplex", "Triplex"],
+        },
+        exposure: {
+          type: "enum",
+          values: ["Northern", "Southern", "Eastern", "Western"],
+        },
+        infrastructureArea: { type: "tuple", items: ["number", "number"] },
+        lenght: { type: "tuple", items: ["number", "number"] },
+        width: { type: "tuple", items: ["number", "number"] },
+        passageWidth: { type: "number", min: 1, max: 1000 },
+        plateType: { type: "enum", values: ["Registered", "Normal"] },
       },
       get: {
         type: "object",
@@ -33,6 +60,19 @@ export const checkPlansDetails = v.compile(schema);
 export interface GetPlansDetails {
   set: {
     pagination: PaginationType;
+    planType: PLANTYPE;
+    units: number;
+    floors: number;
+    sleeps: number;
+    planCode: string;
+    unitType: UNITTYPE;
+    bathroom: number;
+    exposure: POSITION;
+    infrastructureArea: [number, number];
+    lenght: [number, number];
+    width: [number, number];
+    passageWidth: number;
+    plateType: PLATETYPE;
   };
   get: RPlan;
 }
