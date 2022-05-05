06927d3395c915f6c26c43afc362d45b556cb7ae
Syd Amir <hemedani@gmail.com>
2022-05-05 12:16:39 +0430

    log for pathname

--- apps/server/src/utils/serveStatic.ts
+++ apps/server/src/utils/serveStatic.ts
@@ -1,9 +1,40 @@
-import { throwError } from "./throwErr.ts";
 import { readableStreamFromReader } from "https://deno.land/std@0.128.0/streams/mod.ts";
+import { throwError } from "./throwErr.ts";
 // import { addCors } from "../../cors.ts";
 
 export const serveStatic = async (request: Request) => {
+  const url = request.url.split("/");
+  const pathname = url[url.length - 1];
+
+  /*
+  *  @LOG @DEBUG @INFO
+  *  This log written by ::==> {{ syd }}
+  *
+  *  Please remove your log after debugging
+  */
+  console.group("request, pathname ------ ");
+  console.log(" ============= ");
+  console.log();
+  console.info({ request, pathname }, " ------ ");
+  console.log();
+  console.log(" ============= ");
+  console.groupEnd();
+
   const path = `${Deno.cwd()}/files${request.url.split("8080")[1]}`;
+  /*
+  *  @LOG @DEBUG @INFO
+  *  This log written by ::==> {{ syd }}
+  *
+  *  Please remove your log after debugging
+  */
+  console.group("path ------ ");
+  console.log(" ============= ");
+  console.log();
+  console.info({ path }, " ------ ");
+  console.log();
+  console.log(" ============= ");
+  console.groupEnd();
+
   const stats = await Deno.lstat(path);
   const streamFile = async () => {
     const file = await Deno.open(path, { read: true });
