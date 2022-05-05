399ae449edc87d863dbe87dcd96aed58a8eeefee
Syd Amir <hemedani@gmail.com>
2022-05-05 13:54:29 +0430

    fixed server path for upload pdf also

--- apps/client/src/state/actions/file/uploadPdf.ts
+++ apps/client/src/state/actions/file/uploadPdf.ts
@@ -1,34 +1,39 @@
 import axios from "axios";
 import Cookies from "js-cookie";
+import { serverPath } from "./uploadFile";
 
 export const uploadPdf = async (file: File) => {
   const formData = new FormData();
   formData.append("file", file);
-  formData.append("funql-body", JSON.stringify({
-    contents: "streams",
-    wants: {
-      model: "File",
-      doit: "uploadPdf"
-    },
-    details: {
-      set: {},
-      get: {
-        name: 1
-      }
-    }
-  }));
+  formData.append(
+    "funql-body",
+    JSON.stringify({
+      contents: "streams",
+      wants: {
+        model: "File",
+        doit: "uploadPdf",
+      },
+      details: {
+        set: {},
+        get: {
+          name: 1,
+        },
+      },
+    }),
+  );
   try {
     const { data } = await axios.post(
-      "http://127.0.0.1:8080/funql",
+      serverPath,
       formData,
       {
         headers: {
           "Content-Type": "multipart/form-data",
-          token: Cookies.get(process.env.TOKEN)
-        }
-      });
+          token: Cookies.get(process.env.TOKEN),
+        },
+      },
+    );
     return data.body;
   } catch (err) {
     return err;
   }
-}
+};
