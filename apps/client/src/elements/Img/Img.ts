697b9c2de6e9e2ee51e14a983bf38c45ec23a82b
Syd Amir <hemedani@gmail.com>
2022-05-05 11:58:08 +0430

    fixed image path

--- apps/client/src/elements/Img/Img.ts
+++ apps/client/src/elements/Img/Img.ts
@@ -1,8 +1,12 @@
-import styled from 'styled-components';
-import { Media } from 'types';
+import styled from "styled-components";
+import { Media } from "types";
+
+export const imgPath = process.env.NODE_ENV === "development"
+  ? "http://localhost:8000/"
+  : "http://eyvan.satek.ir/statics/";
 
 export const Img = styled.img.attrs<ImgProps>((props) => ({
-  src: props.Src ? (process.env.SRC + props.Src) : props.src,
-})) <ImgProps>``;
+  src: props.Src ? (imgPath + props.Src) : props.src,
+}))<ImgProps>``;
 
 export type ImgProps = Media;
